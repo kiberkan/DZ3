2cc26d9dc05e9afd360ddaab5a6816034b83bb0c
Anton <kiberkan@gmail.com>
2023-04-17 13:44:01 +0600

    Решил задачу 19

---
+++ Example_19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string len = Convert.ToString(number);
+int len1 = len.Length;
+
+if (len1 == 5)
+{
+    if (len[0] == len[4] && len[1] == len[3])
+    {
+        Console.WriteLine("Число: "+number+" - алиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число: "+number+" - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("ERROR: " +number+ " - не является пятизначным");
+}
