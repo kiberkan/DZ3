346ce9cb4db00b86f55acf3efed67d76d797d859
Anton <kiberkan@gmail.com>
2023-04-17 15:12:06 +0600

    Решил задачу 19

--- Example_19/Program.cs
+++ Example_19/Program.cs
@@ -5,23 +5,35 @@
 // 12821 -> да
 // 23432 -> да
 
+string ConvertInString(int number)
+{
+    string len = Convert.ToString(number);
+    return len;
+}
+
+int ConvertInIntLenht(string len)
+{
+    int len1 = len.Length;
+    return len1;
+}
+
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-string len = Convert.ToString(number);
-int len1 = len.Length;
+string len = ConvertInString(number);
+int len1 = ConvertInIntLenht(len);
 
-if (len1 == 5)
-{
-    if (len[0] == len[4] && len[1] == len[3])
+    if (len1 == 5)
     {
+        if (len[0] == len[4] && len[1] == len[3])
+        {
         Console.WriteLine("Число: "+number+" - палиндром");
-    }
+        }
     else
     {
         Console.WriteLine("Число: "+number+" - не палиндром");
     }
-}
-else
-{
+    }
+    else
+    {
     Console.WriteLine("ERROR: " +number+ " - не является пятизначным");
-}
+    }
